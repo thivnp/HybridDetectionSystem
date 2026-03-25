b8b4e882f8e53fcb95d00dd0277c55d0b88ae870
thivnp <thiviruperera129@gmail.com>
2026-03-25 10:37:59 +0530

    Update director.py

--- scripts/director.py
+++ scripts/director.py
@@ -16,7 +16,7 @@ def login():
     password = st.text_input("Password", type="password")
 
     if st.button("Login"):
-        if username == "admin" and password == "1234":
+        if username == "admin" and password == "Qaz@#1234":
             st.session_state["logged_in"] = True
             st.success("Login Successful ✅")
             st.rerun()
