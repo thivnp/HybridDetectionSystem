b206590000a2d04563e9d8b7a165f66124455a38
thivnp <thiviruperera129@gmail.com>
2026-04-05 11:18:25 +0530

    Update director.py

--- scripts/director.py
+++ scripts/director.py
@@ -93,7 +93,7 @@ def analyze_file(filepath):
         elif block_var > 1.5:
             file_type = "Partially Encrypted Image"
 
-        elif entropy > 7.9:
+        elif entropy > 7.96:
             file_type = "Encrypted Image"
 
         elif entropy > 7.5:
